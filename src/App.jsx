d72b2ea82db189c8e183d596c418173a9c1bff1c
Antorkarmokar28 <antorkarmokar28@gmail.com>
2025-12-28 16:14:59 +0600

    Router Setup Done

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <h1>Hello Book Vibe</h1>
-    </>
-  );
-}
-
-export default App;
---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1>Sorry Page not found</h1>
+    </div>
+  );
+};
+
+export default ErrorPage;
